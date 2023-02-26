37f101b0e84008d66782aefd31190fe849ece08d
virginiasatyro <vivi_satyro@hotmail.com>
2023-02-25 21:42:18 -0300

    working version of matrix rain

--- matrix-rain/main.cpp
+++ matrix-rain/main.cpp
@@ -21,14 +21,28 @@ private:
     };
 
     std::list<Streamer> listStreamers;
-    int maxStreamers = 200;
+    int maxStreamers = 250;
 
     void prepareStreamer(Streamer *s)
     {
         s->column = (int)(rand() % ScreenWidth());
         s->position = 0.0f;
         s->speed = rand() % 40 + 5;
-        s->text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        s->text.clear(); // clear string before start to append
+        int streamLength = rand() % 80 + 10; // 10 - 90
+        for(int i = 0; i < streamLength; i++)
+        {
+            s->text.append(1, randomCharacter());
+        }
+        // s->text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    char randomCharacter()
+    {
+        // ASCII 33 - 93
+        return (char)(rand() % 93 + 33); // visiable ASCII character
+        //return (char)(rand() % 0x1EF + 0x00C0); // crazy characets - not very good too many spaces - possible problem with my machine
     }
 
 protected:
@@ -60,8 +74,25 @@ protected:
             for (int i = 0; i < s.text.size(); i++)
             {
                 auto color = s.speed < 15.0f ? olc::DARK_GREEN : olc::GREEN;
+                if(i == 0)
+                {
+                    color = olc::WHITE;
+                }
+                else
+                {
+                    if(i < 3)
+                    {
+                        color = olc::GREY;
+                    }
+                }
+
                 int charIndex = (abs(i - (int)s.position)) % s.text.size();
                 DrawString(s.column, 8 * ((int)s.position - i), std::string(1, s.text[charIndex]), color); // why 8*??
+
+                if (rand() % 1000 < 5)
+                {
+					s.text[i] = randomCharacter();
+                }
             }
 
             if (8 * (s.position - s.text.size()) >= ScreenHeight()) // why 8*??
--- matrix-rain/notes.coffee
+++ matrix-rain/notes.coffee
@@ -6,3 +6,7 @@ compile
 g++ -o main.exe main.cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -static -std=c++17
 
 https://github.com/Joseph21-6147/From-ConsoleGameEngine-to-PixelGameEngine/blob/main/OneLoneCoder_Matrix.PGE.cpp
+
+
+* add Katakana to charactes
+* fix the column overlap
