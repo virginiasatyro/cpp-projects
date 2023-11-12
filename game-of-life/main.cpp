2ce149b4afe18c368d8c7d28c649d7e6834a5968
virginiasatyro <vivi_satyro@hotmail.com>
2023-11-11 23:08:59 -0300

    updating game of life

--- game-of-life/main.cpp
+++ game-of-life/main.cpp
@@ -1,3 +1,35 @@
+/*
+    GAME OF LIFE
+
+    - Based on: https://www.youtube.com/watch?v=E7CxMHsYzSs&list=PLrOv9FMX8xJEEQFU_eAvYTsyudrewymNl&index=54 -  for learning purposes;
+
+    - https://github.com/OneLoneCoder/Javidx9/blob/master/ConsoleGameEngine/SmallerProjects/OneLoneCoder_GameOfLife.cpp
+
+    - https://playgameoflife.com/
+
+    - https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+
+    - https://conwaylife.com/
+
+    -  g++ -o main.exe main.cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -static -std=c++17
+
+    1) count neigbours
+        if cell = alive 
+            <2 dies (lonely)
+            >3 dies (overcrowded)
+
+        if cell = dead
+            =3 alive
+
+    - TODO:
+        - need to treat every cell in one epoch at time
+        - study lambda function
+        - test different sets initializers
+        - https://conwaylife.com/ >> follow this example and color the backgournd as rainbow
+        - what different can we do with cells that go out of screen?
+        - implement Gosper's glider gun 
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -7,8 +39,6 @@
 #define OLC_PGE_APPLICATION
 #include "../commonlib/olcPixelGameEngine.h"
 
-// g++ -o main.exe main.cpp -luser32 -lgdi32 -lopengl32 -lgdiplus -lShlwapi -ldwmapi -lstdc++fs -static -std=c++17
-
 class GameOfLife : public olc::PixelGameEngine
 {
 public:
@@ -16,6 +46,7 @@ public:
     {
         sAppName = "Game Of Life";
     }
+
     int32_t totalScreen = ScreenWidth() * ScreenHeight();
 
 private:
@@ -47,11 +78,11 @@ protected:
             }
         };
 
-       /* // R-Pentonimo
-        set(80, 50, L"  ## ");
-        set(80, 50, L" ##  ");
-        set(80, 50, L" #   ");
-*/
+        /* // R-Pentonimo
+         set(80, 50, L"  ## ");
+         set(80, 50, L" ##  ");
+         set(80, 50, L" #   ");
+ */
         return true;
     }
 
@@ -62,7 +93,7 @@ protected:
             return true; // can be used to stop and see the state
         }*/
         // delay a bit
-        std::this_thread::sleep_for( std::chrono::milliseconds(25));
+        std::this_thread::sleep_for(std::chrono::milliseconds(25));
 
         // lambda function - I need to study it more
         auto cell = [&](int x, int y)
@@ -80,20 +111,21 @@ protected:
         {
             for (int y = 1; y < ScreenHeight() - 1; y++)
             {
-                int neighbours = cell(x - 1, y - 1) + cell(x - 0, y - 1) +
+                int neighbours = cell(x - 1, y - 1) + cell(x + 0, y - 1) +
                                  cell(x + 1, y - 1) + cell(x - 1, y + 0) +
                                  0 + cell(x + 1, y + 0) + cell(x - 1, y + 1) +
                                  cell(x + 0, y + 1) + cell(x + 1, y + 1);
 
                 if (cell(x, y) == 1)
                 {
-                    state[y * ScreenWidth() + x] = neighbours == 2 || neighbours == 3;
+                    state[y * ScreenWidth() + x] = (neighbours == 2) || (neighbours == 3);
                 }
                 else
                 {
-                    state[y * ScreenWidth() + x] = neighbours == 3;
+                    state[y * ScreenWidth() + x] = (neighbours == 3);
                 }
 
+                // Draw
                 if (cell(x, y) == 1) // check if cell is alive
                 {
                     Draw(x, y, olc::WHITE);
