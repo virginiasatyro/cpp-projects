9d46e48346a40aacf4aebb7b9a109551956811de
virginiasatyro <vivi_satyro@hotmail.com>
2024-01-22 22:28:22 -0300

    enhancing fractal tree

--- fractal-trees/main.cpp
+++ fractal-trees/main.cpp
@@ -22,23 +22,18 @@ public:
 
 private:
     float PI = 3.14159f;
-    float angle = PI / 4.0f;
 
     void tree(float x, float y, float length, float angle, float aux)
     {
-        if(length < 4)
+        if (length < 4)
         {
-            std::cout << "Length < 4";
             FillCircle(x, y, 2, olc::RED);
             return;
         }
 
-        float x1 = 0.0;
-        float y1 = 0.0;
+        float x1 = x + length * cosf(PI * angle / 180);
+        float y1 = y - length * sinf(PI * angle / 180);
 
-        x1 = x + (length * cosf(PI * angle / 180));
-        y1 = y - length * sinf(PI * angle / 180);
-        std::cout << "x = " << x << " x1 = " << x1 << "y = " << y << " y1 = " << y1 << std::endl;
         DrawLine(x, y, x1, y1, olc::GREEN);
         tree(x1, y1, length * 0.67, angle - aux, aux);
         tree(x1, y1, length * 0.67, angle - aux, -aux);
@@ -51,17 +46,23 @@ protected:
         PixelGameEngine::ConsoleClear();
         Clear(olc::BLACK);
 
+        // update 
+        float screenWidth = ScreenWidth();
+        float screenHeight = ScreenHeight();
+
         // call tree
-        for(int i = 0; i <=3; i++)
-            tree(64.0, 80, 25, 90, 30);
-            tree(64.0, 80, 25, 90, -30);
+        for (int i = 0; i <= 3; i++)
+        {
+            tree(screenWidth / 2.0, screenHeight, screenHeight / 4.0 + 5, 90, 30);
+            tree(screenWidth / 2.0, screenHeight, screenHeight / 4.0 + 5, 90, -30);
+        }
 
         return true;
     }
 
     // called one per frame
     bool OnUserUpdate(float fElapsedTime) override
-    {   
+    {
         return true;
     }
 };
@@ -69,7 +70,7 @@ protected:
 int main()
 {
     FractalTree demo;
-    if (demo.Construct(128, 80, 4, 4))
+    if (demo.Construct(128, 80, 8, 8))
     {
         demo.Start();
     }
